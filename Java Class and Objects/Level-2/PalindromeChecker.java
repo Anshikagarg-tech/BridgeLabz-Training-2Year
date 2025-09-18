0b59fc608e5b1bfb3eb3298376e1022f038a4887
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-18 19:08:05 +0530

    [Anshika]Added-Program to Check Palindrome String

---
+++ Java Class and Objects/Level-2/PalindromeChecker.java
@@ -0,0 +1,30 @@
+package program.basics;
+
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    public void displayResult() {
+        if (isPalindrome()) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker p1 = new PalindromeChecker("madam");
+        PalindromeChecker p2 = new PalindromeChecker("hello");
+
+        p1.displayResult();
+        p2.displayResult();
+    }
+}
