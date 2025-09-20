baf5d1487393cc60c4f76974d8491414e256b99d
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-20 19:21:29 +0530

    [Anshika]Added-Problem 3: Vehicle and Transport System

---
+++ Inheritance/Assisted Problems/VehicleDemo.java
@@ -0,0 +1,77 @@
+// Superclass
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Vehicle [Max Speed=" + maxSpeed + " km/h, Fuel=" + fuelType + "]");
+    }
+}
+
+// Car subclass
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Car, Seats: " + seatCapacity);
+    }
+}
+
+// Truck subclass
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Truck, Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Motorcycle subclass
+class Motorcycle extends Vehicle {
+    boolean hasGear;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasGear) {
+        super(maxSpeed, fuelType);
+        this.hasGear = hasGear;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Motorcycle, Has Gear: " + (hasGear ? "Yes" : "No"));
+    }
+}
+
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(150, "Petrol", true);
+
+        // Polymorphism: same method called, different behavior
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println("------------------");
+        }
+    }
+}
