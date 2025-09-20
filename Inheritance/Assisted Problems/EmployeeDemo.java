57d36fcac8585aa2430d5bfe236d39bd3955497b
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-20 19:22:09 +0530

    [Anshika]Added-Problem 2: Employee Management System

---
+++ Inheritance/Assisted Problems/EmployeeDemo.java
@@ -0,0 +1,76 @@
+// Base Employee class
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee [ID=" + id + ", Name=" + name + ", Salary=" + salary + "]");
+    }
+}
+
+// Manager subclass
+class Manager extends Employee {
+    int teamSize;
+
+    Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Manager, Team Size: " + teamSize);
+    }
+}
+
+// Developer subclass
+class Developer extends Employee {
+    String programmingLanguage;
+
+    Developer(String name, int id, double salary, String lang) {
+        super(name, id, salary);
+        this.programmingLanguage = lang;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Developer, Language: " + programmingLanguage);
+    }
+}
+
+// Intern subclass
+class Intern extends Employee {
+    int duration; // duration in months
+
+    Intern(String name, int id, double salary, int duration) {
+        super(name, id, salary);
+        this.duration = duration;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Intern, Duration: " + duration + " months");
+    }
+}
+
+public class EmployeeDemo {
+    public static void main(String[] args) {
+        Employee e1 = new Manager("Alice", 101, 80000, 5);
+        Employee e2 = new Developer("Bob", 102, 60000, "Java");
+        Employee e3 = new Intern("Charlie", 103, 20000, 6);
+
+        e1.displayDetails();
+        e2.displayDetails();
+        e3.displayDetails();
+    }
+}
