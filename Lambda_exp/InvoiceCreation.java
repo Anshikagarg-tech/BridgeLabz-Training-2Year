fc44c1e963288e0e4057db28d125edfc76b9b332
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-10-13 12:20:16 +0530

    Anshika[Added]-Program of Invoice Object Creation (Constructor Reference))

---
+++ Lambda_exp/InvoiceCreation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Invoice {
+    String transactionId;
+
+    Invoice(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice created for Transaction ID: " + transactionId;
+    }
+}
+
+public class InvoiceCreation {
+    public static void main(String[] args) {
+        List<String> transactionIds = Arrays.asList("TXN001", "TXN002", "TXN003");
+
+        // Using constructor reference to create Invoice objects
+        List<Invoice> invoices = transactionIds.stream()
+                                               .map(Invoice::new)
+                                               .collect(Collectors.toList());
+
+        invoices.forEach(System.out::println);
+    }
+}
