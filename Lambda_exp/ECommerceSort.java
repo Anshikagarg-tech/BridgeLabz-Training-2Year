101204764f37ef61e49612bb16954e19107fa2ed
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-10-13 12:08:55 +0530

    Anshika[Added]-Program of Custom Sorting in E-Commerce (Using Comparator with Lambda))

---
+++ Lambda_exp/ECommerceSort.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Product {
+    String name;
+    double price;
+    double rating;
+    double discount;
+
+    Product(String name, double price, double rating, double discount) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (₹" + price + ", Rating: " + rating + ", Discount: " + discount + "%)";
+    }
+}
+
+public class ECommerceSort {
+    public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+            new Product("Shoes", 2500, 4.5, 10),
+            new Product("Watch", 1500, 4.8, 20),
+            new Product("Bag", 800, 4.2, 15)
+        );
+
+        // Sort by price
+        products.sort((p1, p2) -> Double.compare(p1.price, p2.price));
+        System.out.println("\nSorted by Price:");
+        products.forEach(System.out::println);
+
+        // Sort by rating
+        products.sort((p1, p2) -> Double.compare(p2.rating, p1.rating));
+        System.out.println("\nSorted by Rating:");
+        products.forEach(System.out::println);
+
+        // Sort by discount
+        products.sort((p1, p2) -> Double.compare(p2.discount, p1.discount));
+        System.out.println("\nSorted by Discount:");
+        products.forEach(System.out::println);
+    }
+}
