2bed4cd704d72def88fa100fb4bd01e2ad00d00a
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-11-02 22:00:36 +0530

    Anshika[Added] - problem solution of Shopping Cart Price Calculation

---
+++ Wrapper class/ShoppingCart.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        String prices[] = {"250", "499", "99", "abc", "150"};
+        int total = 0;
+
+        for(String p : prices) {
+            try {
+                total += Integer.parseInt(p); // string → int
+            } catch(NumberFormatException e) {
+                System.out.println("Invalid price skipped: " + p);
+            }
+        }
+        System.out.println("Total Price = " + total);
+    }
+}
+
